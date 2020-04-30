d46ff8bfb8e1aa2d584b552a87d44cb7168aa770
Sajjan Vaidya <sajjanrajvaidya@gmail.com>
2020-04-29 21:45:13 -0700

    Setup the basic structure of the application folder

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// THIS IS AFTER REMOVING ALL THE COMMENTS
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const port = 8080;
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Serving is now listening on port: ${port}`);
+})
