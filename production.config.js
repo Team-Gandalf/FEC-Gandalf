af062b3767810d103aba1a9ac2a52127e383df32
Sajjan Vaidya <sajjanrajvaidya@gmail.com>
2020-05-14 11:45:24 -0700

    Add some optimizations

---
+++ production.config.js
@@ -0,0 +1,45 @@
+// webpack.config.js
+const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+    concatenateModules: true,
+  },
+  entry: './client/',
+  output: {
+    path: path.resolve(__dirname, './public/dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          // Inline files smaller than 10 kB (10240 bytes)
+          limit: 10 * 1024,
+        },
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        loader: 'image-webpack-loader',
+        // This will apply the loader before the other ones
+        enforce: 'pre',
+      },
+    ],
+  },
+};
